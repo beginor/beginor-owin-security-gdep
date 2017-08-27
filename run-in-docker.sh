d6016b79f72caec2baf4368d38989f50a1dfef5a
beginor <beginor@qq.com>
2017-08-27 12:06:41 +0800

    update run script

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-docker run \
+docker run --rm \
   --interactive \
   --tty \
   --volume $(pwd)/test/GdepTest:/var/www/default \
   --volume $(pwd)/jexus/default:/usr/jexus/siteconf/default \
   --publish 8088:80 \
-  beginor/jexus-x64:5.8.2.20
+  --name jexus-test \
+  beginor/jexus-x64:5.8.3-RC1
